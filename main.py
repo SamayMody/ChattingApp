f02fc8ae13ef8f72d651ff2a044b1b608c01b1d8
Srmsamay <sm7525@srmist.edu.in>
2024-04-23 13:05:48 +0530

    Built chatting app 🗣️

---
+++ main.py
@@ -0,0 +1,88 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <h2>Your ID: <span id="ws-id"></span></h2>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var client_id = Date.now()
+            document.querySelector("#ws-id").textContent = client_id;
+            var ws = new WebSocket(`ws://localhost:8000/ws/${client_id}`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connection: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connection.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connection.remove(websocket)
+
+    async def sendMessages(self, message: str, websocket: WebSocket):
+        await websocket.send_text(message)
+
+    async def broadcastMessage(self, message: str):
+        for connection in self.active_connection:
+            await connection.send_text(message)
+
+
+manager = ConnectionManager()
+
+
+@app.get("/")
+async def get():
+    return HTMLResponse(html)
+
+
+@app.websocket("/ws/{client_id}")
+async def websocket_endpoint(websocket: WebSocket, client_id: int):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            #await manager.sendMessages(f"You wrote {data}", websocket)
+            await manager.broadcastMessage(f"Client {client_id} messaged: {data} ")
+
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)
+        await manager.disconnect(f"Client {client_id} left the chat")
+
+
+
+
+
+
